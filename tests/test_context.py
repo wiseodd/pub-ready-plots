39b239e4d6bb31ef94931fb5476960a25af64eca
Agustinus Kristiadi <agustinus@kristia.de>
2024-08-15 12:48:38 -0400

    Fix `pytest` path

--- tests/test_context.py
+++ tests/test_context.py
@@ -4,10 +4,9 @@ import numpy as np
 
 def test_correct_func():
     nrows, ncols = 3, 2
-    fig, axs, rc_params = get_context(0.5, 0.15, nrows, ncols, "iclr")
+    fig, axs = get_context(0.5, 0.15, nrows, ncols, "iclr")
     real_rc_params, fig_width_in, fig_height_in = get_mpl_rcParams(0.5, 0.15, "iclr")
 
-    assert rc_params == real_rc_params
     assert np.allclose(fig.get_size_inches(), (fig_width_in, fig_height_in), atol=0.001)
     assert isinstance(axs, np.ndarray)
     assert axs.shape == (nrows, ncols)
