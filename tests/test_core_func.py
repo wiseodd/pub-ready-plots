9da5221327536a1b91888ae0229895cb52a722af
Agustinus Kristiadi <agustinus@kristia.de>
2024-08-23 17:02:24 -0400

    Add test for double column option

--- tests/test_core_func.py
+++ tests/test_core_func.py
@@ -7,14 +7,14 @@ LAYOUTS = ["icml", "iclr", "neurips", "jmlr", "poster-portrait", "poster-landsca
 
 
 @pytest.mark.parametrize("layout", LAYOUTS)
-def test_correct_func(layout):
+def test_correct_func(layout: str) -> None:
     rc_params, width_in, height_in = get_mpl_rcParams(
         width_frac=1, height_frac=0.15, layout=layout
     )
     plt.rcParams.update(rc_params)
 
 
-def test_incorrect_width():
+def test_incorrect_width() -> None:
     with pytest.raises(ValueError):
         _, _, _ = get_mpl_rcParams(width_frac=12, height_frac=0.1, layout="iclr")
 
@@ -25,7 +25,7 @@ def test_incorrect_width():
         _, _, _ = get_mpl_rcParams(width_frac=-3.2, height_frac=0.1, layout="iclr")
 
 
-def test_incorrect_height():
+def test_incorrect_height() -> None:
     with pytest.raises(ValueError):
         _, _, _ = get_mpl_rcParams(width_frac=0.15, height_frac=12, layout="iclr")
 
@@ -36,8 +36,29 @@ def test_incorrect_height():
         _, _, _ = get_mpl_rcParams(width_frac=0.15, height_frac=-1.2, layout="iclr")
 
 
-def test_incorrect_layout():
+def test_incorrect_layout() -> None:
     with pytest.raises(ValueError):
         _, _, _ = get_mpl_rcParams(
             width_frac=0.5, height_frac=0.5, layout="predatory_journal"
         )
+
+
+@pytest.mark.parametrize("layout", LAYOUTS)
+def test_double_column(layout: str) -> None:
+    if layout == "icml":
+        _ = get_mpl_rcParams(
+            width_frac=1, height_frac=0.1, layout=layout, single_col=False
+        )
+
+        _ = get_mpl_rcParams(
+            width_frac=1, height_frac=0.1, layout=layout, single_col=True
+        )
+    else:
+        _ = get_mpl_rcParams(
+            width_frac=1, height_frac=0.1, layout=layout, single_col=False
+        )
+
+        with pytest.raises(ValueError):
+            _ = get_mpl_rcParams(
+                width_frac=1, height_frac=0.1, layout=layout, single_col=True
+            )
